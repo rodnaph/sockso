f43fea9e1f85aa5f4546a156beaafe51229be8c2
rod <rod@pu-gh.com>
2011-01-30 14:25:10 +0000

    implemented useradd command

--- src/com/pugh/sockso/CommandExecuter.java
+++ src/com/pugh/sockso/CommandExecuter.java
@@ -3,6 +3,8 @@ package com.pugh.sockso;
 
 import com.pugh.sockso.db.Database;
 import com.pugh.sockso.music.CollectionManager;
+import com.pugh.sockso.resources.Locale;
+import com.pugh.sockso.web.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -49,11 +51,14 @@ public class CommandExecuter {
     
     private final CollectionManager cm;
 
-    public CommandExecuter( final Database db, final Properties p, final CollectionManager cm ) {
+    private final Locale locale;
+
+    public CommandExecuter( final Database db, final Properties p, final CollectionManager cm, final Locale locale ) {
 
         this.db = db;
         this.p = p;
         this.cm = cm;
+        this.locale = locale;
 
     }
 
@@ -72,17 +77,78 @@ public class CommandExecuter {
         final String name = args.length > 0 ? args[0] : "";
 
         if ( name.equals(CMD_USERLIST) ) { return cmdUserList(); }
-        else if (name.equals(CMD_USERLIST)) { return cmdUserAdd(args); }
+        else if (name.equals(CMD_USERADD)) { return cmdUserAdd(args); }
 
         return null;
 
     }
 
-    protected String cmdUserAdd( final String[] args ) {
+    /**
+     *  Tries to add a new user
+     *
+     *  @param args
+     *
+     *  @return
+     *
+     *  @throws SQLException
+     *
+     */
+    
+    protected String cmdUserAdd( final String[] args ) throws SQLException {
 
-        return "";
+        if ( args.length != 5 ) {
+            return getCommands();
+        }
+        
+        else {
+
+            try {
+
+                final Validater v = new Validater( db );
+                final String name = args[ 1 ];
+                final String pass = args[ 2 ];
+                final String email = args[ 3 ];
+                final String isAdmin = args[ 4 ];
+
+                if ( v.usernameExists(name) ) {
+                    throw new ValidationException( locale.getString("con.err.usernameExists") );
+                }
+
+                if ( v.emailExists(email) ) {
+                    throw new ValidationException( locale.getString("con.err.emailExists") );
+                }
+
+                final User newUser = new User(
+                    name,
+                    pass,
+                    email,
+                    isAdmin.equals("1") ? true : false
+                );
+
+                newUser.save( db );
+
+                return newUser.getId() != -1
+                    ? locale.getString( "con.msg.userCreated")
+                    : locale.getString( "con.err.errorCreatingUser");
+
+            }
+
+            catch ( final ValidationException e ) {
+                return e.getMessage();
+            }
+
+        }
 
     }
+
+    /**
+     *  Lists current users
+     *
+     *  @return
+     *
+     *  @throws SQLException
+     *
+     */
     
     protected String cmdUserList() throws SQLException {
 
@@ -101,9 +167,12 @@ public class CommandExecuter {
             rs = st.executeQuery();
 
             while ( rs.next() ) {
-                sb.append( rs.getString("id") + "\t" );
-                sb.append( rs.getString("name") + "\t" );
-                sb.append( rs.getString("email") + "\t" );
+                sb.append( rs.getString("id")  );
+                sb.append( "\t" );
+                sb.append( rs.getString("name") );
+                sb.append( "\t" );
+                sb.append( rs.getString("email") );
+                sb.append( "\t" );
                 sb.append( rs.getBoolean("is_admin") ? "ADMIN" : "" );
                 sb.append( "\n" );
             }
@@ -131,6 +200,30 @@ public class CommandExecuter {
     protected String[] getArgs( final String command ) {
 
         return command.split( " " );
+
+    }
+
+    protected String getCommands() {
+
+        final StringBuffer sb = new StringBuffer();
+        
+        sb.append( locale.getString("con.desc.commands") );
+        sb.append( CMD_COLADD + " PATH                            " + locale.getString("con.desc.addDirectory") );
+        sb.append( CMD_COLDEL + " PATH                            " + locale.getString("con.desc.removeDirectory") );
+        sb.append( CMD_COLLIST + "                                " + locale.getString("con.desc.listDirectories") );
+        sb.append( CMD_COLSCAN + "                                " + locale.getString("con.desc.rescanCollection") );
+        sb.append( CMD_EXIT + "                                   " + locale.getString("con.desc.exit") );
+        sb.append( CMD_PROPLIST + " FILTER                        " + locale.getString("con.desc.listProperties") );
+        sb.append( CMD_PROPSET + " NAME VALUE                     " + locale.getString("con.desc.setProperty") );
+        sb.append( CMD_PROPDEL + " NAME                           " + locale.getString("con.desc.delProperty") );
+        sb.append( CMD_USERLIST + "                               " + locale.getString("con.desc.listUsers") );
+        sb.append( CMD_USERADD + " NAME PASS EMAIL ISADMIN (1/0)  " + locale.getString("con.desc.addUser") );
+        sb.append( CMD_USERDEL + " ID                             " + locale.getString("con.desc.deleteUser") );
+        sb.append( CMD_USERADMIN + " ID ISADMIN (1/0)             " + locale.getString("con.desc.adminUser") );
+        sb.append( CMD_VERSION + "                                " + locale.getString("con.desc.version") );
+        sb.append( "" );
+
+        return sb.toString();
 
     }
 
--- test/com/pugh/sockso/CommandExecuterTest.java
+++ test/com/pugh/sockso/CommandExecuterTest.java
@@ -2,10 +2,12 @@
 package com.pugh.sockso;
 
 import com.pugh.sockso.db.Database;
+import com.pugh.sockso.resources.Locale;
 import com.pugh.sockso.web.User;
 
 import com.pugh.sockso.tests.SocksoTestCase;
 import com.pugh.sockso.tests.TestDatabase;
+import com.pugh.sockso.tests.TestLocale;
 
 public class CommandExecuterTest extends SocksoTestCase {
 
@@ -15,11 +17,17 @@ public class CommandExecuterTest extends SocksoTestCase {
     
     private Database db;
 
+    private TestLocale locale;
+
     @Override
     public void setUp() {
         p = new StringProperties();
         db = new TestDatabase();
-        cmd = new CommandExecuter( db, p, null );
+        locale = new TestLocale();
+        locale.setString( "con.msg.userCreated", "user added" );
+        locale.setString( "con.desc.commands", "Usage:" );
+        locale.setString( "con.err.usernameExists", "username exists" );
+        cmd = new CommandExecuter( db, p, null, locale );
     }
 
     public void testListingUsersReturnsTheirDetails() throws Exception {
@@ -30,7 +38,7 @@ public class CommandExecuterTest extends SocksoTestCase {
         assertContains( result, user.getName() );
         assertContains( result, user.getEmail() );
     }
-    
+
     public void testListingUsersIncludesAdminIfTheyAreAnAdmin() throws Exception {
         User user = new User( "foo", "", "foo@bar.com", true );
         user.save( db );
@@ -66,12 +74,34 @@ public class CommandExecuterTest extends SocksoTestCase {
         assertFalse( User.find( db, 0 ).isAdmin() );
     }
 
-    public void testUserReportedAsHavingBeenAddedWhenTheyAre() {
-
+    public void testUserReportedAsHavingBeenAddedWhenTheyAre() throws Exception {
+        String message = cmd.execute( "useradd name pass email@domain.com 0" );
+        assertContains( message, "user added" );
     }
 
-    public void testUserReportedTextUsesLocale() {
-        
+    public void testUserReportedTextUsesLocale() throws Exception {
+        String message = cmd.execute( "useradd name pass email@domain.com 0" );
+        assertEquals( message, "user added" );
+    }
+
+    public void testUsageReturnedWhenTooManyArgumentsPassedToUseradd() throws Exception {
+        assertContains( cmd.execute("useradd foo bar email@domain.com 0 asd qwe"), "Usage:" );
+    }
+
+    public void testUsageReturnedWhenNotEnoughArgumentsPassedToUseradd() throws Exception {
+        assertContains( cmd.execute("useradd foo bar email@domain.com"), "Usage:" );
+    }
+
+    public void testAddingAUserWithDuplicateUsernameDoesNotCreateThem() throws Exception {
+        cmd.execute( "useradd foo bar email@domain.com 0" );
+        cmd.execute( "useradd foo bar email@domain.com 0" );
+        assertTableSize( db, "users", 1 );
+    }
+
+    public void testAddingAUserWithDuplicateUsernameReturnsTheErrorMessage() throws Exception {
+        cmd.execute( "useradd foo bar email@domain.com 0" );
+        String message = cmd.execute( "useradd foo bar email@domain.com 0" );
+        assertEquals( message, "username exists" );
     }
 
 }
