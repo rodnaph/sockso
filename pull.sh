aab514a02242d3c8a71cde13672743267a907eab
rod <rod@pu-gh.com>
2011-03-18 10:50:51 +0000

    removed uneeded pull/push scripts

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Pulling website..."
-cd website
-git pull
-
-echo "Pulling master..."
-cd ../
-git pull
--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Pushing website..."
-cd website
-git push origin master
-
-echo "Pushing master..."
-cd ../
-git push origin master
