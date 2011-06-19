7f2cbb2cc963b6d4daed9b88ec6b788a59f7f3c4
rod <rod@pu-gh.com>
2011-06-19 15:44:40 +0100

    removed old test

--- test/com/pugh/sockso/web/action/PlayerTest.java
+++ test/com/pugh/sockso/web/action/PlayerTest.java
@@ -86,22 +86,6 @@ public class PlayerTest extends SocksoTestCase {
         
     }
 
-    public void testShowJsPlayer() throws Exception {
-        
-        final TestResponse res = new TestResponse();
-        final Player player = new Player();
-        final Vector<Track> tracks = new Vector<Track>();
-        
-        player.setProperties( new StringProperties() );
-        player.setResponse( res );
-        player.showJsPlayer( tracks );
-        
-        final String data = res.getOutput();
-        
-        assertTrue( data.length() > 0 );
-        
-    }
-
     public void testShowHtml5Player() throws Exception {
         
         final TestResponse res = new TestResponse();
