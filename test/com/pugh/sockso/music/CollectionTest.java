08352fb91f7455326fccb8569216bdd009de6b1c
Nathan Perrier <nathan.perrier@gmail.com>
2011-10-05 20:48:42 -0500

    Revert "Fixed unit tests that were failing when run on windoze"
    This reverts commit 7307b5695020c5858b3554d19847925815ce3028.

--- test/com/pugh/sockso/music/CollectionTest.java
+++ test/com/pugh/sockso/music/CollectionTest.java
@@ -1,3 +1,4 @@
+
 package com.pugh.sockso.music;
 
 import com.pugh.sockso.db.Database;
@@ -7,77 +8,63 @@ import com.pugh.sockso.tests.TestDatabase;
 import java.sql.SQLException;
 
 public class CollectionTest extends SocksoTestCase {
-
+    
     private Database db;
-    private static final String os = System.getProperty("os.name").toLowerCase();
-
+    
     @Override
     public void setUp() throws Exception {
         db = new TestDatabase();
-        db.update("insert into collection ( id, path ) values ( 1, '/foo/bar/' )");
-        db.update("insert into collection ( id, path ) values ( 2, 'C:\\foo\\bar\\' )");
+        db.update( "insert into collection ( id, path ) values ( 1, '/foo/bar/' )" );
+        db.update( "insert into collection ( id, path ) values ( 2, 'C:\\foo\\bar\\' )" );
     }
 
     public void testConstructors() {
         final int id = 123;
         final String path = "/home/me/music";
-        assertNotNull(new Collection());
-        assertNotNull(new Collection(id, path));
+        assertNotNull( new Collection() );
+        assertNotNull( new Collection(id,path) );
     }
 
     public void testGetters() {
         final int id = 123;
         final String path = "/home/me/music";
-        final Collection col = new Collection(id, path);
-        assertEquals(id, col.getId());
-        assertEquals(path, col.getPath());
+        final Collection col =  new Collection(id,path);
+        assertEquals( id, col.getId() );
+        assertEquals( path, col.getPath() );
     }
 
     public void testCollectionCanBeFetchedWhereItContainsAPath() throws SQLException {
-        if (os.contains("mac") || os.contains("linux")) {
-            Collection c = Collection.findByPath(db, "/foo/bar/baz/");
-            assertEquals(1, c.getId());
-        }
+        Collection c = Collection.findByPath( db, "/foo/bar/baz/" );
+        assertEquals( 1, c.getId() );
     }
-
+    
     public void testCollectionCanBeFetchedWhereItContainsAPathWithoutTrailingSlash() throws SQLException {
-        if (os.contains("mac") || os.contains("linux")) {
-            Collection c = Collection.findByPath(db, "/foo/bar/baz");
-            assertEquals(1, c.getId());
-        }
+        Collection c = Collection.findByPath( db, "/foo/bar/baz" );
+        assertEquals( 1, c.getId() );
     }
-
+    
     public void testCollectionReturnedWhenPathMatchesRootWithoutTrailingSlash() throws SQLException {
-        if (os.contains("mac") || os.contains("linux")) {
-            Collection c = Collection.findByPath(db, "/foo/bar");
-            assertEquals(1, c.getId());
-        }
+        Collection c = Collection.findByPath( db, "/foo/bar" );
+        assertEquals( 1, c.getId() );
     }
 
     public void testCollectionReturnedWhenPathMatchesRootWithTrailingSlash() throws SQLException {
-        if (os.contains("mac") || os.contains("linux")) {
-            Collection c = Collection.findByPath(db, "/foo/bar/");
-            assertEquals(1, c.getId());
-        }
+        Collection c = Collection.findByPath( db, "/foo/bar/" );
+        assertEquals( 1, c.getId() );
     }
 
     public void testCollectionReturnedWhenPathMatchesRootWithWindowsPathAndTrailingSlash() throws SQLException {
-        if (os.contains("windows")) {
-            Collection c = Collection.findByPath(db, "C:\\foo\\bar\\");
-            assertEquals(2, c.getId());
-        }
+        Collection c = Collection.findByPath( db, "C:\\foo\\bar\\" );
+        assertEquals( 2, c.getId() );
     }
 
     public void testCollectionReturnedWhenPathMatchesRootWithWindowsPathAndNoTrailingSlash() throws SQLException {
-        if (os.contains("windows")) {
-            Collection c = Collection.findByPath(db, "C:\\foo\\bar");
-            assertEquals(2, c.getId());
-        }
+//        Collection c = Collection.findByPath( db, "C:\\foo\\bar" );
+//        assertEquals( 2, c.getId() );
     }
 
     public void testNullReturnedWhenTryingToFindACollectionByPathDoesntMatchAnything() throws SQLException {
-        if (os.contains("mac") || os.contains("linux")) {
-            assertNull(Collection.findByPath(db, "/bar/baz"));
-        }
+        assertNull( Collection.findByPath( db, "/bar/baz" ) );
     }
+    
 }
