500f6cb9e0d9ac7e85631127bfdf9733d2fc5d90
rod <rod@pu-gh.com>
2011-12-10 14:28:55 +0000

    added www.metatags property namespace for adding arbitrary meta tags (fixes #80)

--- test/com/pugh/sockso/tests/TemplateTestCase.java
+++ test/com/pugh/sockso/tests/TemplateTestCase.java
@@ -1,41 +1,30 @@
 
 package com.pugh.sockso.tests;
 
-import com.pugh.sockso.Properties;
-import com.pugh.sockso.web.User;
-
 import org.jamon.Renderer;
 
 public abstract class TemplateTestCase extends SocksoTestCase {
 
-    public String render( final Properties p ) {
+    /**
+     *  Render the template and return the result as a string
+     * 
+     *  @return 
+     * 
+     */
+    
+    public String render() {
 
-        return render( p, null );
-
-    }
-
-    public String render( final Properties p, final User user ) {
-
-        return getTemplate( p, user ).asString();
-
-    }
-
-    public Renderer getTemplate( final Properties p ) {
-
-        return getTemplate( p, null );
+        return getTemplate().asString();
 
     }
 
     /**
      *  Returns the renderer for the template that will be under test
      *
-     *  @param p
-     *  @param user
-     *
      *  @return
      *
      */
 
-    public abstract Renderer getTemplate( final Properties p, final User user );
+    public abstract Renderer getTemplate();
 
 }
