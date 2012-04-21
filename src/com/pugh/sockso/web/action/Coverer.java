fbbb6a6a50ba3be302f6d7889b1237b0ba9b1902
rod <rod@pu-gh.com>
2012-04-21 18:53:13 +0100

    added some comments

--- src/com/pugh/sockso/web/action/Coverer.java
+++ src/com/pugh/sockso/web/action/Coverer.java
@@ -11,17 +11,34 @@ public class Coverer extends BaseAction {
 
     private final ArrayList<CovererPlugin> plugins;
 
+    /**
+     *  Create a new coverer action
+     * 
+     */
+
     public Coverer() {
         
         this.plugins = new ArrayList<CovererPlugin>();
         
     }
 
+    /**
+     *  Add a plugin, these are evaluated in order added
+     * 
+     *  @param plugin 
+     * 
+     */
+
     public void addPlugin( CovererPlugin plugin ) {
 
         plugins.add( plugin );
 
     }
+
+    /**
+     *  When handling a request just try and find a plugin to serve the cover
+     * 
+     */
 
     public void handleRequest() {
         
@@ -33,12 +50,26 @@ public class Coverer extends BaseAction {
 
     }
 
+    /**
+     *  No login required when serving covers, same as FileServer
+     * 
+     *  @return 
+     * 
+     */
+
     @Override
     public boolean requiresLogin() {
 
         return false;
         
     }
+
+    /**
+     *  No login means no session required at all
+     * 
+     *  @return
+     * 
+     */
 
     @Override
     public boolean requiresSession() {
