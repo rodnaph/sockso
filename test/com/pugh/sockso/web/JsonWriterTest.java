e7be970cec98964091f872d150f012a473e867fd
rod <rod@pu-gh.com>
2012-07-17 19:25:58 +0100

    added test to try and reproduce issue 108

--- test/com/pugh/sockso/web/JsonWriterTest.java
+++ test/com/pugh/sockso/web/JsonWriterTest.java
@@ -7,12 +7,28 @@ import java.io.StringWriter;
 
 public class JsonWriterTest extends SocksoTestCase {
 
-    public void testWhiteSpaceIsRemovedFromJson() throws Exception {
-        String json = "{  id: 123, name: \"foo \\\"bar\" }";
-        StringWriter writer = new StringWriter();
-        JsonWriter jsonWriter = new JsonWriter( writer );
+    private StringWriter stringWriter;
+    private JsonWriter jsonWriter;
+
+    @Override
+    protected void setUp() {
+        stringWriter = new StringWriter();
+        jsonWriter = new JsonWriter( stringWriter );
+    }
+
+    protected String write( String json ) throws Exception {
         jsonWriter.write( json );
-        assertEquals( "{id:123,name:\"foo \\\"bar\"}", writer.toString() );
+        return stringWriter.toString();
+    }
+
+    public void testWhiteSpaceIsRemovedFromJson() throws Exception {
+        String input = "{  id: 123, name: \"foo \\\"bar\" }";
+        assertEquals( "{id:123,name:\"foo \\\"bar\"}", write(input) );
+    }
+
+    public void testIssue109() throws Exception {
+        String input = "{  \"name\":   \"vinyl 12\\\"\",    \n  \"id\"    :    123 }";
+        assertEquals( "{\"name\":\"vinyl 12\\\"\",\"id\":123}", write(input) );
     }
     
 }
