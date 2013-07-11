f7e3ea04d774511162f19a8cb6ac93569bc13627
Nathan Perrier <nathan.perrier@gmail.com>
2013-07-10 22:55:48 -0500

    Add api endpoint for Genres.  Fix bug with SQL in Genre

--- src/com/pugh/sockso/music/Genre.java
+++ src/com/pugh/sockso/music/Genre.java
@@ -1,7 +1,12 @@
 package com.pugh.sockso.music;
 
+import com.pugh.sockso.Utils;
+import com.pugh.sockso.db.Database;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.log4j.Logger;
 
 /**
@@ -13,7 +18,7 @@ public class Genre extends MusicItem {
     private static final Logger log = Logger.getLogger(Genre.class);
     
     /**
-     *
+     * @param id
      * @param name
      */
     public Genre( final int id, final String name ) {
@@ -29,7 +34,56 @@ public class Genre extends MusicItem {
      */
     public static Genre createFromResultSet( final ResultSet rs ) throws SQLException {
 
-        return new Genre(rs.getInt("id"), rs.getString("genre"));
+        return new Genre(rs.getInt("id"), rs.getString("name"));
     }
-    
+
+    /**
+     *  Find all genres, listed alphabetically, with the specified offset and limit
+     *
+     *  @param db
+     *  @param limit
+     *  @param offset
+     *
+     *  @throws SQLException
+     *
+     *  @return
+     *
+     */
+
+    public static List<Genre> findAll( final Database db, final int limit, final int offset ) throws SQLException {
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            String sql = " select g.id, g.name " +
+                               " from genres g " +
+                               " order by g.name asc ";
+
+            if ( limit != -1 ) {
+                sql += " limit " +limit+ " " +
+                       " offset " +offset;
+            }
+
+            st = db.prepare( sql );
+            rs = st.executeQuery();
+
+            final List<Genre> genres = new ArrayList<Genre>();
+
+            while ( rs.next() ) {
+                Genre genre = createFromResultSet(rs);
+                genres.add( genre );
+            }
+            
+            return genres;
+
+        }
+
+        finally {
+            Utils.close( rs );
+            Utils.close( st );
+        }
+
+    }
 }
